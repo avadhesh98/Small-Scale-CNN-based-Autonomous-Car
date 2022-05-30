f3e67c5ec19b89c8a3c05d64c451b9b512f411e5
Avadhesh Rathi <avadheshrathi98@gmail.com>
2022-05-30 03:44:09 -0700

    9th June

--- preprocess.py
+++ preprocess.py
@@ -9,7 +9,7 @@ import cv2
 import params
 
 import local_common as cm
-
+from PIL import Image, ImageFilter
 ################ parameters ###############
 img_height = params.img_height
 img_width = params.img_width
@@ -23,9 +23,9 @@ def preprocess(img):
     padding = int(round((img.shape[1] - w) / 2))
     # img = img[y1:y2, padding:-padding]
     img = cv2.resize(img, (img_width, img_height))
-
-    res = cv2.imwrite('/home/avadhesh/thesis/test.jpg',img)
-    print(res)
+    #img2 = cv2.medianBlur(img,5)
+    #res = cv2.imwrite('/home/avadhesh/thesis/test.jpg',img)
+    #print(res)
 
     img = img / 255.00
     return img
